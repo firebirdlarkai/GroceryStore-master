112274c48d7f8c48b66561e0e90e70523f36da2b
neelratanguria <neelratan@live.com>
2020-04-21 18:28:24 +0530

    Initial commit

---
+++ app/src/main/java/com/quintus/labs/grocerystore/interfaces/AddorRemoveCallbacks.java
@@ -0,0 +1,11 @@
+package com.quintus.labs.grocerystore.interfaces;
+
+
+public interface AddorRemoveCallbacks {
+
+    void onAddProduct();
+
+    void onRemoveProduct();
+
+    void updateTotalPrice();
+}
